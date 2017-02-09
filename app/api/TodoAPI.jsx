ba0260fdfd96461982fc2e480e9f67e0dd520fe6
Ethan Bellora <Belloramail@gmail.com>
2017-02-09 13:04:48 -0600

    added the filter API

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -20,5 +20,32 @@ module.exports = {
             console.log(e);
         }
         return $.isArray(todos) ? todos: [];
+    },
+    filterTodos: function(todos, showCompleted, searchText) {
+        var filterTodos = todos;
+        
+        //filter by showCompleted
+        filterTodos = filterTodos.filter((todo) => {
+            return !todo.completed || showCompleted;
+        });
+
+        //filter by searchText
+        if(searchText != undefined){
+            filterTodos = filterTodos.filter((todo) => {
+                
+                return todo.text.toLowerCase().includes(searchText);
+            });
+        }
+        // sort todos with non-completed first
+        filterTodos.sort((a,b) => {
+            if(a.completed === false && b.completed === true){
+                return -1;
+            } else if (a.completed === true && b.completed === false){
+                return 1;
+            } else {
+                return 0;
+            }
+        })
+        return filterTodos;
     }
 };
