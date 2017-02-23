250773350d03e54341ec8e45f98f65506fe40d35
Ethan Bellora <Belloramail@gmail.com>
2017-02-23 12:10:49 -0500

    fixed button rendering

--- app/components/Login.jsx
+++ app/components/Login.jsx
@@ -16,14 +16,16 @@ export var Login = React.createClass({
             <div>
                 <h1 className="page-title">Todo React Application</h1>
                 <div className="row">
-                    <div className="columns small-centered small-10 medium-6 large-4">
+                    <div className="columns small-centered small-10 medium-6">
                         <div className="callout callout-off">
                             <h3>Login</h3>
                             <p>
                                 Login securely with a social provider below.
                             </p>
-                            <button className="button" onClick={this.onGitLogin}>Login With GitHub</button>
-                            <button className="button" onClick={this.onTwitterLogin}>Login With Twitter</button>
+                           
+                                <button className="button" onClick={this.onGitLogin}>Login With GitHub</button>
+                                <button className="button" onClick={this.onTwitterLogin}>Login With Twitter</button>
+                           
                             <p>
                                 This allows for secure login by allowing a 3rd party to handle user data.
                             </p>
